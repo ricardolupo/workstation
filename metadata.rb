39f8f82d8557f2d550b423cf142eeef5abf52810
ricardolupo <rlupo@getchef.com>
2018-10-25 09:44:57 -0700

    bump metadata

--- metadata.rb
+++ metadata.rb
@@ -1,22 +1,22 @@
 name 'workstation'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
+maintainer 'Ricardo Lupo'
+maintainer_email 'Ricardo@chef.io'
+license 'Apache v2'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.2.1'
+version '0.2.2'
 chef_version '>= 13.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/workstation/issues'
+issues_url 'https://github.com/ricardolupo/workstation/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/workstation'
+source_url 'https://github.com/ricardolupo/workstation'
 depends 'homebrew'
 depends 'chocolatey'
